f841dc1d5efa88ede0fcdffbab79acb51dd29624
Ahmad Alkaff <ahmadalkaff3003@gmail.com>
2017-10-17 21:11:20 +0800

    Add newline at EOF for test

--- src/test/java/seedu/address/logic/commands/ListTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListTagCommandTest.java
@@ -117,5 +117,4 @@ public class ListTagCommandTest {
         expectedMessage = MESSAGE + tempList.toString().replace(",", "]").replace(" ", " [");
         assertCommandSuccess(listTagCommand, model, expectedMessage, expectedModel);
     }
-
 }
