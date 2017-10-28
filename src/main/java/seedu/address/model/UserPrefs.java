93dde35b1d2acdd0e54f34ff9a0cddde47a20cd6
Ahmad Alkaff <ahmadalkaff3003@gmail.com>
2017-10-28 21:15:01 +0800

    Minor change

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -10,7 +10,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs {
 
     private GuiSettings guiSettings;
-    private String addressBookFilePath = "data/contacthub.xml";
+    private String addressBookFilePath = "data/addressbook.xml";
     private String addressBookName = "ContactHub";
 
     public UserPrefs() {
